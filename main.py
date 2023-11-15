622eb1357ff013e3b1271365f90be0dc28dfa4b2
Yukino0113 <jethro0113@gmail.com>
2023-11-15 22:53:19 +0800

    Edited for test use

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import sys
 from PyQt5.QtWidgets import QApplication
+
+from UI.HeadshotWindow import HeadshotWindow
 from UI.startWindow import StartWindow
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    window = StartWindow()
+    # window = StartWindow()
+    window = HeadshotWindow()
     window.show()
     sys.exit(app.exec_())

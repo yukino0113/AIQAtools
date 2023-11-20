32000bd254ffefbeb07eaa12d0e75655e8f27123
Yukino0113 <jethro0113@gmail.com>
2023-11-20 13:57:48 +0800

    Fixed var name to camelCase

--- src/Controller.py
+++ src/Controller.py
@@ -14,9 +14,9 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         self.setup_control()
 
     def setup_control(self):
-        self.ui.ImportOpenBtn.clicked.connect(self.import_path)
-        self.ui.ExportOpenBtn.clicked.connect(self.export_path)
-        self.ui.LoadPath.clicked.connect(self.load_path)
+        self.ui.importOpenBtn.clicked.connect(self.import_path)
+        self.ui.exportOpenBtn.clicked.connect(self.export_path)
+        self.ui.loadPath.clicked.connect(self.load_path)
         self.ui.nextPic.clicked.connect(self.next_image)
 
         self.ui.importPath.setText("C:\\Users\\jethro_wang\\Desktop")
@@ -29,14 +29,13 @@ class MainWindow_controller(QtWidgets.QMainWindow):
     def export_path(self) -> None:
         folder_path = QFileDialog.getExistingDirectory(self, 'Select a folder')
         if folder_path:
-            self.ui.ExportPath.setText(folder_path)
+            self.ui.exportPath.setText(folder_path)
 
     def load_path(self) -> None:
         if not self.ui.importPath.text():
             QMessageBox.critical(None, "Error", "請先指定輸入路徑")
         else:
             image_dict = self.get_generated_path()
-            ic(image_dict)
 
     def get_generated_path(self) -> dict:
         image_dict = {}
@@ -54,5 +53,5 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         return image_dict
 
     def next_image(self):
-        if not self.ui.ExportPath.text():
+        if not self.ui.exportPath.text():
             QMessageBox.critical(None, "Error", "請先指定輸出路徑")

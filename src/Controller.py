285c2883be355756290873a050e0bef11d53d08c
Yukino0113 <jethro0113@gmail.com>
2023-11-21 13:52:01 +0800

    new func

--- src/Controller.py
+++ src/Controller.py
@@ -1,14 +1,14 @@
-import os
-from icecream import ic
 from PyQt6 import QtWidgets
 from PyQt6.QtWidgets import QFileDialog, QMessageBox
 
 from src.UI.Ui_Dialog import Ui_Dialog
+from src.function.GeneratedImage import GeneratedImage
 
 
 class MainWindow_controller(QtWidgets.QMainWindow):
     def __init__(self):
         super().__init__()
+        self.genImage = None
         self.ui = Ui_Dialog()
         self.ui.setupUi(self)
         self.setup_control()
@@ -35,23 +35,33 @@ class MainWindow_controller(QtWidgets.QMainWindow):
         if not self.ui.importPath.text():
             QMessageBox.critical(None, "Error", "請先指定輸入路徑")
         else:
-            image_dict = self.get_generated_path()
+            self.genImage = GeneratedImage(self.ui.importPath.text())
 
-    def get_generated_path(self) -> dict:
-        image_dict = {}
-        source_path = self.ui.importPath.text()
-        for style_folder in [x for x in os.listdir(source_path)
-                             if (os.path.isdir(os.path.join(source_path, x)) and 'Design' in x)]:
-            image_dict[style_folder] = {}
-            for source_folder in style_folder:
-                path = os.path.join(source_path, style_folder)
-                source_folder = ic(x for x in os.listdir(path) if os.path.isdir(os.path.join(path, x)))
-                for folder in source_folder:
-                    os.chdir(os.path.join(path, folder))
-                    for file in os.listdir(os.path.join(path, folder)):
-                        image_dict[style_folder][file.split('.')[0]] = os.path.join(path, folder, file)
-        return image_dict
+    def load_generated_path(self):
+        # todo
+        #   create image showing class
+        #   can get showing number and can modify
+        #   can get the image path from the number and load image
+        pass
+
+    def load_reference_image(self):
+        # todo
+        #  name parser for generated image name
+        #  loading image from reference folder (Need class?)
+        pass
+
+    def previous_image(self):
+        pass
+
+    def pass_image(self):
+        # todo
+        #  will pop hint window?
+        pass
 
     def next_image(self):
         if not self.ui.exportPath.text():
             QMessageBox.critical(None, "Error", "請先指定輸出路徑")
+        # todo
+        #  if no CB, will pop window to tell user wil save to no issue folder.
+
+

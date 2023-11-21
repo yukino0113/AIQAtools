ba7e2049e3cdcc42cbc2ef9e196a80a7a2217fef
Yukino0113 <jethro0113@gmail.com>
2023-11-21 17:15:45 +0800

    done load image

--- src/Controller.py
+++ src/Controller.py
@@ -1,5 +1,8 @@
-from PyQt6 import QtWidgets
+import os
+
+from PyQt6 import QtWidgets, QtGui, QtCore
 from PyQt6.QtWidgets import QFileDialog, QMessageBox
+from icecream import ic
 
 from src.UI.Ui_Dialog import Ui_Dialog
 from src.function.GeneratedImage import GeneratedImage
@@ -36,35 +39,42 @@ class MainWindowController(QtWidgets.QMainWindow):
             QMessageBox.critical(None, "Error", "請先指定輸入路徑")
         else:
             self.genImage = GeneratedImage(self.ui.importPath.text())
+            self.load_image()
 
-    def load_generated_path(self):
-        # todo
-        #   create image showing class
-        #   can get showing number and can modify
-        #   can get the image path from the number and load image
-        pass
+    def load_image(self):
+        gen_scene = QtWidgets.QGraphicsScene()
+        gen_scene.addPixmap(QtGui.QPixmap(self.genImage.get_current_image_path()))
+        self.ui.generatedPic.setScene(gen_scene)
 
-    def load_reference_image(self):
-        # todo
-        #  name parser for generated image name
-        #  loading image from reference folder (Need class?)
-        pass
+        self.ui.fileNameLabel.setText(self.genImage.currentImage)
+
+        ic(os.path.dirname(os.path.realpath(__file__)))
+        ref_file_path = ic(f'{os.path.dirname(os.path.realpath(__file__))}\\..\\reference_image\\'
+                           f'{"_".join(self.genImage.currentImage.split("_")[:2])}.jpg')
+        ref_scene = QtWidgets.QGraphicsScene()
+        ref_scene.addPixmap(QtGui.QPixmap(ref_file_path))
+        self.ui.referencePic.setScene(ref_scene)
 
     def previous_image(self):
         self.genImage.previous()
+
+        self.load_image()
         pass
 
     def pass_image(self):
         self.genImage.next()
         # todo
         #  will pop hint window?
-        pass
+
+        self.load_image()
 
     def next_image(self):
-        if not self.ui.exportPath.text():
-            QMessageBox.critical(None, "Error", "請先指定輸出路徑")
-        else:
-            self.genImage.next()
         # todo
         #  if no CB, will pop window to tell user wil save to no issue folder.
+
+        #if not self.ui.exportPath.text():
+        #    QMessageBox.critical(None, "Error", "請先指定輸出路徑")
+        #else:
+            self.genImage.next()
+            self.load_image()
 
--- src/function/GeneratedImage.py
+++ src/function/GeneratedImage.py
@@ -6,34 +6,49 @@ from icecream import ic
 class GeneratedImage:
     def __init__(self, path):
         self.path = path
-        self.imagePathDic = self.get_generated_path()
-        self.styleOrder = 0
+        self.imageMaxCount = 0
+        self.imagePathDic = self._get_path()
+        # self.styleOrder = 0
         self.ImageOrder = 0
-        self.currentStyle = list(self.imagePathDic.keys())[self.styleOrder]
+        self.currentStyle = list(self.imagePathDic.keys())[self.ImageOrder // 80]
         self.currentImage = list(self.imagePathDic[self.currentStyle].keys())[self.ImageOrder]
-        self.testVar = 0
 
-    def get_generated_path(self) -> dict:
+        # self.testVar = ic(self.ImageOrder)
+
+    def _refresh_current_style_and_image(self):
+        self.currentStyle = list(self.imagePathDic.keys())[self.ImageOrder // 80]
+        self.currentImage = list(self.imagePathDic[self.currentStyle].keys())[self.ImageOrder]
+
+    def _get_path(self) -> dict:
         image_dict = {}
         for styleFolder in [x for x in os.listdir(self.path)
-                             if (os.path.isdir(os.path.join(self.path, x)) and 'Design' in x)]:
+                            if (os.path.isdir(os.path.join(self.path, x)) and 'Design' in x)]:
             image_dict[styleFolder] = {}
             for source_folder in styleFolder:
                 path = os.path.join(self.path, styleFolder)
-                source_folder = ic(x for x in os.listdir(path) if os.path.isdir(os.path.join(path, x)))
+                source_folder = [x for x in os.listdir(path) if os.path.isdir(os.path.join(path, x))]
                 for folder in source_folder:
                     os.chdir(os.path.join(path, folder))
                     for file in os.listdir(os.path.join(path, folder)):
                         image_dict[styleFolder][file.split('.jpg')[0]] = os.path.join(path, folder, file)
+            self.imageMaxCount += len(list(image_dict[styleFolder].keys()))
         return image_dict
 
     def next(self):
-        if self.ImageOrder < len(list(self.imagePathDic[self.currentStyle].keys())):
+        if self.ImageOrder < self.imageMaxCount - 1:
             self.ImageOrder += 1
+            self._refresh_current_style_and_image()
+        else:
+            # todo: error handling
+            pass
 
     def previous(self):
         if self.ImageOrder > 0:
             self.ImageOrder -= 1
+            self._refresh_current_style_and_image()
+        else:
+            # todo: error handling
+            pass
 
-
-GeneratedImage("C:\\Users\\jethro_wang\\Desktop")
+    def get_current_image_path(self) -> str:
+        return self.imagePathDic[self.currentStyle][self.currentImage]

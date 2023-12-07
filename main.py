3b103cc827a9d0bde0246456d0291caebf7d91d0
yukino0113 <jethro0113@gmail.com>
2023-12-07 17:46:17 +0800

    Add clear logs when starting the tool

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from PyQt6 import QtWidgets
 import sys
 
 from src.Controller import MainWindowController
+from src.function.Logging import Logging
+
+
+Logging().clear_log()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)

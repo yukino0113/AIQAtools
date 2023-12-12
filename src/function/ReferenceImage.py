4be032bfd4b798b0c06a4e30236887f6bc4ca55e
Yukino0113 <jethro0113@gmail.com>
2023-12-12 23:37:25 +0800

    Add log

--- src/function/ReferenceImage.py
+++ src/function/ReferenceImage.py
@@ -1,13 +1,15 @@
 import os
+from ..function.Logging import Logging
 from icecream import ic
+
 
 class ReferenceImage:
 
     def __init__(self, path):
         self.imagePathList = self._get_path(path)
+        self.log = Logging()
 
-    @staticmethod
-    def _get_path(path) -> dict:
+    def _get_path(self, path) -> dict:
 
         image = {}
 
@@ -15,5 +17,5 @@ class ReferenceImage:
         for file in os.listdir(path):
             if file.endswith('.png') or file.endswith('.jpg'):
                 image[os.path.basename(file).split('.')[0]] = os.path.join(path, file)
-
+        self.log.log('Load Reference Image', f'Loaded reference image from {path}')
         return image

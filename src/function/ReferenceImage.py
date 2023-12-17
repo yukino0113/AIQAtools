2ba0d3bb53cd0ac699f4960bcd16c0796162ab27
yukino0113 <jethro0113@gmail.com>
2023-12-17 17:52:14 +0800

    Change ref's log  calling

--- src/function/ReferenceImage.py
+++ src/function/ReferenceImage.py
@@ -5,8 +5,8 @@ from icecream import ic
 
 class ReferenceImage(Logging):
 
-    def __init__(self, path):
-        super().__init__()
+    def __init__(self, path, log_path):
+        self.log_path = os.path.join(os.path.dirname(log_path), 'result', 'log.txt')
         self.imagePathList = self._get_path(path)
 
     def _get_path(self, path) -> dict:
@@ -17,5 +17,5 @@ class ReferenceImage(Logging):
         for file in os.listdir(path):
             if file.endswith('.png') or file.endswith('.jpg'):
                 image[os.path.basename(file).split('.')[0]] = os.path.join(path, file)
-        self.log('Load Reference Image', f'Loaded reference image from {path}')
+        self.log_message('Load Reference Image', f'Loaded reference image from {path}', self.log_path)
         return image

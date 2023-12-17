5db681ae2ceffb665e3a51c298c6d32f501242c8
yukino0113 <jethro0113@gmail.com>
2023-12-17 17:50:53 +0800

    Change logs logic

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from PyQt6 import QtWidgets
 import sys
 
 from src.Controller import MainWindowController
-from src.function.Logging import Logging
-
-Logging().clear_log()
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)

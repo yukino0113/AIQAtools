e8b8d5354f38caca9cd6cfd543f7c6f08217eb2d
yukino0113 <jethro0113@gmail.com>
2023-12-17 17:56:35 +0800

    Added logging function for quitting

--- src/function/Logging.py
+++ src/function/Logging.py
@@ -7,10 +7,18 @@ class Logging:
     def get_time():
         return datetime.now().strftime("%H:%M:%S")
 
-    def clear_log(self):
-        open(self.log_path, 'w').close()
-
-    def log(self, function, message):
+    def log_message(self, function, message, log_path):
         time = datetime.now().strftime("%H:%M:%S")
-        with open(self.log_path, 'a') as f:
-            f.write(f'[{time} {function}] {message}\n')
+        with open(log_path, 'a+', encoding='utf-8') as f:
+            f.write(f'[{self.get_time()}] [{function}] {message}\n')
+
+    def log_end(self, log_path):
+        with open(log_path, 'a+', encoding='utf-8') as f:
+            f.write(f'[{self.get_time()}] [Test End] [準備輸出摘要]\n')
+            f.write(f'[{self.get_time()}] [] \n')
+            f.write(f'[{self.get_time()}] [] \n')
+            f.write(f'[{self.get_time()}] [] \n')
+
+    def log_summary(self, issue, quantity, log_path):
+        with open(log_path, 'a+', encoding='utf-8') as f:
+            f.write(f'[{self.get_time()}] [Summary] {issue}: {quantity}\n')
